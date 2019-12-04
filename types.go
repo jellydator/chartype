e4cd53303eb361522db4a75ed6d57864abefb03d
simon <simk@jellydator.dev>
2019-12-04 16:18:48 +0000

    ticker functions

--- types.go
+++ types.go
@@ -27,7 +27,7 @@ const (
 type Candle struct {
 	Open  decimal.Decimal `json:"open"`
 	High  decimal.Decimal `json:"high"`
-	Low   decfmal.Decimal `json:"low"`
+	Low   decimal.Decimal `json:"low"`
 	Close decimal.Decimal `json:"close"`
 }
 
@@ -49,12 +49,12 @@ func (cf CandleField) MarshalJSON() ([]byte, error) {
 	case CandleClose:
 		return []byte("close"), nil
 	default:
-		return nil, errors.New("undefined candle type")
+		return nil, errors.New("undefined candle field")
 	}
 }
 
-// UnmarshalJSON turns JSON string to appropriate field
-// value.
+// UnmarshalJSON turns JSON string to appropriate candle
+// field value.
 func (cf *CandleField) UnmarshalJSON(d []byte) error {
 	var f string
 	if err := json.Unmarshal(d, &f); err != nil {
@@ -79,7 +79,7 @@ func (cf *CandleField) UnmarshalJSON(d []byte) error {
 	return nil
 }
 
-// Extract returns candle's value specified in the candle
+// Extract returns candle's value as specified in the candle
 // field type.
 func (cf CandleField) Extract(c Candle) decimal.Decimal {
 	switch cf {
@@ -96,6 +96,17 @@ func (cf CandleField) Extract(c Candle) decimal.Decimal {
 	}
 }
 
+const (
+	// TickerLast specifies the last ticker value.
+	TickerLast TickerField = iota + 1
+
+	// TickerAsk specifies the ask ticker value.
+	TickerAsk
+
+	// TickerBid specifies the bid ticker value.
+	TickerBid
+)
+
 // Ticker holds current ask, last and bid prices.
 type Ticker struct {
 	Last decimal.Decimal
@@ -103,7 +114,66 @@ type Ticker struct {
 	Bid  decimal.Decimal
 }
 
-// Packet holds tickert information as well as all
+// TickerField specifies which field should be extracted
+// from the ticker for further calculations.
+// Can be included in configuration structures.
+type TickerField int
+
+// MarshalJSON turns ticker field to appropriate string
+// representation in JSON.
+func (tf TickerField) MarshalJSON() ([]byte, error) {
+	switch tf {
+	case TickerLast:
+		return []byte("last"), nil
+	case TickerAsk:
+		return []byte("ask"), nil
+	case TickerBid:
+		return []byte("bid"), nil
+	default:
+		return nil, errors.New("undefined ticker field")
+	}
+}
+
+// UnmarshalJSON turns JSON string to appropriate ticker
+// field value.
+func (tf *TickerField) UnmarshalJSON(d []byte) error {
+	var t string
+	if err := json.Unmarshal(d, &t); err != nil {
+		return err
+	}
+
+	t = strings.ToLower(t)
+
+	switch t {
+	case "last", "l":
+		*tf = TickerLast
+	case "ask", "a":
+		*tf = TickerAsk
+	case "bid", "b":
+		*tf = TickerBid
+	default:
+		return errors.New("undefined ticker field")
+	}
+
+	return nil
+}
+
+// Extract returns ticker's value as specified in the ticker
+// field type.
+func (tf TickerField) Extract(t Ticker) decimal.Decimal {
+	switch tf {
+	case TickerLast:
+		return t.Last
+	case TickerAsk:
+		return t.Ask
+	case TickerBid:
+		return t.Bid
+	default:
+		return decimal.Zero
+	}
+}
+
+// Packet holds ticker information as well as all
 // known candles for a specific timeframe.
 type Packet struct {
 	Ticker  Ticker
